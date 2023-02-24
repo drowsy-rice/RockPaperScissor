1611a46115205ca950cd299202847793606917ea
David <davidjusticejimenez@gmail.com>
2023-02-24 03:35:46 -0600

    Assign styling and flexbox properties to html elements Reword game terms to reflect pokemon typing Reverse scoring logic so points count down to zero and loser is announced

--- rps.js
+++ rps.js
@@ -1,22 +1,22 @@
-const rockButton=document.querySelector('#rockButton');
-rockButton.addEventListener('click',function(){
-    let playerSelection="rock";
+const fireButton=document.querySelector('#fireButton');
+fireButton.addEventListener('click',function(){
+    let playerSelection="fire";
     let computerSelection=getComputerChoice();
     playRound(playerSelection,computerSelection);
     finalResults();
 });
 
-const paperButton=document.querySelector('#paperButton');
-paperButton.addEventListener('click',function(){
-    let playerSelection="paper";
+const waterButton=document.querySelector('#waterButton');
+waterButton.addEventListener('click',function(){
+    let playerSelection="water";
     let computerSelection=getComputerChoice();
     playRound(playerSelection,computerSelection);
     finalResults();
 });
 
-const scissorsButton=document.querySelector('#scissorsButton');
-scissorsButton.addEventListener('click',function(){
-    let playerSelection="scissors";
+const grassButton=document.querySelector('#grassButton');
+grassButton.addEventListener('click',function(){
+    let playerSelection="grass";
     let computerSelection=getComputerChoice();
     playRound(playerSelection,computerSelection);
     finalResults();
@@ -24,12 +24,12 @@ scissorsButton.addEventListener('click',function(){
 
 const resetButton=document.querySelector('#resetButton');
     resetButton.addEventListener('click', function(){
-        playerScore=0; computerScore=0;
-        h1.textContent = `COMPUTER SCORE: ${computerScore}`;
-        h2.textContent = `PLAYER SCORE: ${playerScore}`;
-        h3.textContent="Choose Wisely";
+        playerScore=5; computerScore=5;
+        h1.textContent = `ROCKET GRUNT'S HP: ${computerScore}`;
+        h2.textContent = `YOUR HP: ${playerScore}`;
+        document.querySelector('#messages').textContent="Choose Wisely";
         document.querySelector('#starters').style.visibility="visible";
-}) 
+}); 
 
 function getComputerChoice(){
     let min=1
@@ -37,54 +37,52 @@ function getComputerChoice(){
         rNumber=Math.floor(Math.random()*(max-min+1)+min);
    
     if (rNumber==1){
-        return "rock";
+        return "fire";
     }else if (rNumber==2){
-        return "paper";
+        return "water";
     }else
-        return "scissors";
+        return "grass";
 };
 
-let computerScore=0;
-let playerScore=0;
+let computerScore=5;
+let playerScore=5;
 
 function playRound(playerSelection,computerSelection){
     if (playerSelection==computerSelection){
-        h3.textContent=("It's a tie?!");
-    }else if (playerSelection=="rock" && computerSelection=="paper"){
-        computerScore++; h3.textContent=("You lost! Paper beats rock, YOU OAF.");
-    }else if (playerSelection=="rock" && computerSelection=="scissors"){
-        playerScore++; h3.textContent=("You won! Rock beats scissors.");
-    }else if (playerSelection=="paper" && computerSelection=="rock"){
-        playerScore++; h3.textContent=("You won! Paper beats rock.");
-    }else if (playerSelection=="paper" && computerSelection=="scissors"){
-        computerScore++; h3.textContent=("You lost! Scissors beats paper, FOOL.");
-    }else if (playerSelection=="scissors" && computerSelection=="rock"){
-        computerScore++; h3.textContent=("You lost! Rock beats paper, GET GUD LOL.");
-    }else if (playerSelection=="scissors" && computerSelection=="paper"){
-        playerScore++; h3.textContent=("You won! Scissors beats paper.");
+        messages.textContent=("Stop copying me and pick a different Pokemon you twerp!");
+    }else if (playerSelection=="fire" && computerSelection=="water"){
+        computerScore--; messages.textContent=("Ha! I win this round! Water beats fire, YOU OAF!");
+    }else if (playerSelection=="fire" && computerSelection=="grass"){
+        playerScore--; messages.textContent=("You got lucky that time! Fire beats grass.");
+    }else if (playerSelection=="water" && computerSelection=="rock"){
+        playerScore--; messages.textContent=("You won that?? Water beats fire.");
+    }else if (playerSelection=="water" && computerSelection=="grass"){
+        computerScore--; messages.textContent=("Of course you lost that! Grass beats water, FOOL.");
+    }else if (playerSelection=="grass" && computerSelection=="fire"){
+        computerScore--; messages.textContent=("Don't you know fire beats grass?? GET GUD LOL.");
+    }else if (playerSelection=="grass" && computerSelection=="water"){
+        playerScore--; messages.textContent=("I let you win that! Grass beats water.");
     }
-    h1.textContent = `COMPUTER SCORE: ${computerScore}`;
-    h2.textContent = `PLAYER SCORE: ${playerScore}`;
+    h1.textContent = `ROCKET GRUNT'S HP: ${computerScore}`;
+    h2.textContent = `YOUR HP: ${playerScore}`;
 };
 
 function finalResults(){
-    if (playerScore==5){
-        h3.textContent=("YOU WIN!!!!!!!"); document.querySelector('#starters').style.visibility="hidden";
-    }else if (computerScore==5){
-        h3.textContent=("YOU HAVE FAILED EVERYONE, TRY AGAIN!"); document.querySelector('#starters').style.visibility="hidden";
+    if (playerScore==0){
+        messages.textContent="HAHAHA! TEAM ROCKET IS VICTORIOUS!"; document.querySelector('#starters').style.visibility="hidden";
+    }else if (computerScore==0){
+        messages.textContent="You defeated me?? I'm telling Giovanni about this you brat..."; document.querySelector('#starters').style.visibility="hidden";
 }};
 
 const h1=document.createElement('h1');
-h1.textContent=`COMPUTER SCORE: ${computerScore}`;
+h1.textContent=`ROCKET GRUNT'S HP: ${computerScore}`;
 h1.style.border='thick solid red';
+h1.style.backgroundColor='white';
 scores.appendChild(h1);
 
 const h2=document.createElement('h1');
-h2.textContent=`PLAYER SCORE: ${playerScore}`;
+h2.textContent=`YOUR HP: ${playerScore}`;
 h2.style.border='thick solid green';
+h2.style.backgroundColor='white';
 scores.appendChild(h2);
 
-const h3=document.createElement('h1');
-h3.textContent="Choose Wisely"
-h3.style.border='thick solid black';
-scores.appendChild(h3);
